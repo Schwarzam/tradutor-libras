4de8fbfb0fc916492987ef31fb57f0f1e94cde03
schwarzam <gustavo.b.schwarz@gmail.com>
2021-05-07 20:11:16 -0300

    updated to loop on MainLoop

--- mainLoop.py
+++ mainLoop.py
@@ -1,3 +1,5 @@
+##ssh -L 5901:127.0.0.1:5901 -C -N -l desktop 192.168.0.111
+
 from keras.models import load_model
 import numpy as np
 from extracao_dados import ler_imagens  ## Tem que estar na mesma pasta do arquivo extracao_dados.py
@@ -11,7 +13,11 @@ labels = le.transform(labels)
 
 modelo = load_model('rede_neural.h5')  ## tem que estar na mesma pasta do arquivo
 
-imagem_pronta = tirar_foto()
-resposta = modelo.predict(imagem_pronta)
-resposta = np.argmax(resposta, axis=-1) ## Vai gerar o numero correspondente a resposta
-print(le.classes_[resposta[0]])
+x = True
+while x:
+	imagem_pronta = tirar_foto()
+	resposta = modelo.predict(imagem_pronta)
+	resposta = np.argmax(resposta, axis=-1) ## Vai gerar o numero correspondente a resposta
+	print(le.classes_[resposta[0]])
+
+	sleep(0.4)
